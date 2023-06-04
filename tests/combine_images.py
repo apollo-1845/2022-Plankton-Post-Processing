6c564a0fcf48992bb33d4e104c3c11bfc26118e1
WebCoder49 <webcoder49.tutorials@gmail.com>
2023-06-04 20:21:48 +0100

    More work on image combiner but still not completely accurate

--- tests/combine_images.py
+++ tests/combine_images.py
@@ -35,7 +35,7 @@ def display_matches(image_1, keypoints_1, image_2, keypoints_2, matches):
 
 def get_movement_vector(image_1, image_2):
     # Get keypoints > matches
-    keypoints_1, descriptors_1, keypoints_2, descriptors_2 = calculate_features(image_1, image_2, 1000)
+    keypoints_1, descriptors_1, keypoints_2, descriptors_2 = calculate_features(image_1, image_2, 10000)
     matches = calculate_matches(descriptors_1, descriptors_2)
     # display_matches(image_1, keypoints_1, image_2, keypoints_2, matches)
     # Remove some with longer distance as seems to make more accurate
@@ -48,27 +48,47 @@ def get_movement_vector(image_1, image_2):
         match = matches[i]
         coords_1 = keypoints_1[match.queryIdx].pt
         coords_2 = keypoints_2[match.trainIdx].pt
-        x_movements[i] = coords_2[0]-coords_1[0]
-        y_movements[i] = coords_2[1]-coords_1[1]
+        x_movements[i] = coords_1[0]-coords_2[0]
+        y_movements[i] = coords_1[1]-coords_2[1]
     # Mean vector
-    return np.mean(x_movements, dtype=int), np.mean(y_movements, dtype=int)
+    return np.mean(x_movements).astype(int), np.mean(y_movements).astype(int)
     # return np.median(x_movements).astype(int), np.median(y_movements).astype(int)
 
-def combine_images(image_1, image_2, x_movement, y_movement):
-    # TODO: Make it work with other +/- sign x_/y_movements.
-    # Move image 2 so it is in the correct position, with image 1 starting at the top-left corner
-    image_2 = np.hstack((image_2, np.zeros((image_2.shape[0], x_movement, image_2.shape[2]), dtype=image_2.dtype))) # Shape is y, x, depth
-    image_2 = np.vstack((np.zeros((-y_movement, image_2.shape[1], image_2.shape[2]), dtype=image_2.dtype), image_2)) # Shape is y, x, depth
+def combine_images(image_1_x_offset, image_1_y_offset, image_1, image_2, x_movement, y_movement):
+    # TODO: Test + fix weird sign change / inaccuracies of movement finder.
 
-    # Fill image 1's mask black area with the part of image 2 in its place, then place image 1 at the top corner
-    image_2[0:image_1.shape[0], x_movement:x_movement+image_1.shape[1]] = np.maximum(image_1, cv.bitwise_and(CAMERA_COVER_MASK, image_2[0:image_1.shape[0], x_movement:x_movement+image_1.shape[1]]))
-    # image_2[0:image_1.shape[0], x_movement:x_movement+image_1.shape[1]] = np.maximum(image_1, image_2[0:image_1.shape[0], x_movement:x_movement+image_1.shape[1]])
+    x_offset = image_1_x_offset
+    y_offset = image_1_y_offset
 
-    # Image 2 now refers to the combined image
-    return image_2
+    # Move image 1 so it is in the correct position, with image 2 starting at the top-left corner
+    if(x_movement+x_offset < 0):  # Moving beyond left side of image
+        image_1 = np.hstack((np.zeros((image_1.shape[0], -x_movement-x_offset, image_1.shape[2]), dtype=image_1.dtype), image_1)) # Shape is y, x, depth
+    elif(x_movement+x_offset > (image_1.shape[1]-image_2.shape[1])):  # Moving beyond right side of image
+        x_offset += x_movement
+        image_1 = np.hstack((image_1, np.zeros((image_1.shape[0], x_movement+x_offset-(image_1.shape[1]-image_2.shape[1]), image_1.shape[2]), dtype=image_1.dtype)))  # Shape is y, x, depth
+    if (y_movement+y_offset < 0):  # Moving beyond top of image
+        image_1 = np.vstack((np.zeros((-y_movement-y_offset, image_1.shape[1], image_1.shape[2]), dtype=image_1.dtype), image_1)) # Shape is y, x, depth
+    elif(y_movement+y_offset > (image_1.shape[0]-image_2.shape[0])):  # Moving beyond bottom of image
+        y_offset += y_movement
+        image_1 = np.vstack((image_1, np.zeros((y_movement+y_offset-(image_1.shape[0]-image_2.shape[0]), image_1.shape[1], image_1.shape[2]), dtype=image_1.dtype))) # Shape is y, x, depth
 
-image_1 = cv.imread("data/63.png") # 0 flags
-image_2 = cv.imread("data/64.png") # 0 flags
-x_movement, y_movement = get_movement_vector(image_1, image_2)
-print(x_movement, y_movement)
-cv.imwrite("data/temp.png", combine_images(image_1, image_2, x_movement, y_movement))
+    # Fill image 2's mask black area with the part of image 1 in its place, then place image 2 at the top corner
+    print(y_offset, y_offset+image_2.shape[0], x_offset, x_offset+image_2.shape[1], image_1.shape, image_1[y_offset:y_offset+image_2.shape[0], x_offset:x_offset+image_2.shape[1]].shape, image_2.shape, CAMERA_COVER_MASK.shape)
+    image_1[y_offset:y_offset+image_2.shape[0], x_offset:x_offset+image_2.shape[1]] = np.maximum(image_2, cv.bitwise_and(CAMERA_COVER_MASK, image_1[y_offset:y_offset+image_2.shape[0], x_offset:x_offset+image_2.shape[1]]))
+    # image_1[y_offset:y_offset+image_2.shape[0], x_offset:x_offset+image_2.shape[1]] = np.maximum(image_2, image_1[y_offset:y_offset+image_2.shape[0], x_offset:x_offset+image_2.shape[1]])
+
+    # Image 1 now refers to the combined image
+    return image_1
+
+images = list((cv.imread(f"data/{i}.png")) for i in range(79, 81))
+
+
+x_movement, y_movement = 0, 0
+result = images[0]
+for i in range(1, len(images)):
+    x_movement_old, y_movement_old = x_movement, y_movement
+    x_movement, y_movement = get_movement_vector(images[i-1], images[i])
+    print(x_movement, y_movement)
+    result = combine_images(0 if x_movement_old < 0 else x_movement_old, 0 if y_movement_old < 0 else y_movement_old, result, images[i], x_movement, y_movement)
+
+cv.imwrite("data/temp.png", result)
